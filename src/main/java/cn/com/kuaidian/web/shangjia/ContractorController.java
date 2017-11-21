5ca9c11f3be6fa20b4f469ed76cb457583005614
huangwe1lian <33713011+huangwe1lian@users.noreply.github.com>
2017-11-21 17:45:43 +0800

    代码调整

---
+++ src/main/java/cn/com/kuaidian/web/shangjia/ContractorController.java
@@ -0,0 +1,5 @@
+package cn.com.kuaidian.web.shangjia;
+
+public class ContractorController {
+
+}
---
+++ src/main/java/cn/com/kuaidian/web/user/UserController.java
@@ -0,0 +1,10 @@
+/**
+ * 
+ */
+/**
+ * @name:
+ * @return
+ * @author user
+ * @date 2017年11月21日
+ */
+package cn.com.kuaidian.web.user;
