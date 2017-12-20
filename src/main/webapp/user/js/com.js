d483a03b6a67dfaa14cbd657a0a8cf348e446aac
huangwe1lian <33713011+huangwe1lian@users.noreply.github.com>
2017-12-20 17:25:00 +0800

    页面提交

--- src/main/webapp/user/js/com.js
+++ src/main/webapp/user/js/com.js
@@ -4,3 +4,20 @@ com.tips = function(str) {
 		alert: str
 	})
 }
+com.setCookie=function (name,value) 
+{ 
+    var Days = 30; 
+    var exp = new Date(); 
+    exp.setTime(exp.getTime() + Days*24*60*60*1000); 
+    document.cookie = name + "="+ escape (value) + ";expires=" + exp.toGMTString(); 
+} 
+com.getCookie=function(name) 
+{ 
+    var arr,reg=new RegExp("(^| )"+name+"=([^;]*)(;|$)");
+ 
+    if(arr=document.cookie.match(reg))
+ 
+        return unescape(arr[2]); 
+    else 
+        return null; 
+} 
